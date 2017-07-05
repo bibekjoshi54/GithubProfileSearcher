4c1a9ca8df187f1d1e2679de9b9db6565d3af58f
Bibek Joshi <bibekjoshi54@gmail.com>
2017-07-05 20:45:17 +0545

    Added the github services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from 'app/profile/profile.component';
-
+import {GithubService} from 'app/service/github.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GithubService]
+
 })
 export class AppComponent {
   c: ProfileComponent;
